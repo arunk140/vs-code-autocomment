cd7e178e42eea561f58afadfe3ae04cf81aabc18
Arun <arunk140@gmail.com>
2021-11-13 11:17:01 +1300

    Show Information Message onActivate with a Button to Open Settings

--- src/extension.ts
+++ src/extension.ts
@@ -9,23 +9,37 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vs-code-autocomment" is now active!');
+	if (!isCodexKeySet()) {
+		vscode.window.showInformationMessage('Please set the Open AI API Key in the settings', 'Open Settings').then(selection => {
+		if (selection === 'Open Settings') {
+			vscode.commands.executeCommand('workbench.action.openSettings', 'vs-code-autocomment');
+		}});
+	} else {
+		vscode.window.showInformationMessage('Hello World from vs-code-autocomment!');
+	}
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vs-code-autocomment.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		var extConfig = vscode.workspace.getConfiguration('vs-code-autocomment');
-		if (extConfig.get('codexKey') === null || extConfig.get('codexKey') === '') {
-			vscode.commands.executeCommand('workbench.action.openSettings', 'vs-code-autocomment');
-			vscode.window.showInformationMessage('Please set the Open AI API Key in the settings');
-		} else {
-			vscode.window.showInformationMessage('Hello World from vs-code-autocomment!');
-		}
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+// A Function to check if Codex Key is set
+function isCodexKeySet() {
+	var extConfig = vscode.workspace.getConfiguration('vs-code-autocomment');
+	console.log(extConfig);
+	if (extConfig.get('codexKey') === null || extConfig.get('codexKey') === '') {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
